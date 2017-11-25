9a3a2a723b800293bb5e7a2a9430ece5a8d3cd48
rosie w <rosie.wilt@gmail.com>
2017-11-25 01:01:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function kittens() {
+var kittens = ["Milo", "Otis", "Garfield"]
+}
+
+function destructivelyAppendKitten() {
+  kittens.push("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+
+function removeLastKitten(){
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.slice(3)
+  return kittens
+}
+
+function removeFirstKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.slice(0)
+  return kittens
+}
+
+
+function appendKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens = [...kittens, "Broom"]
+  return kittens
+}
+
+function prependKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens = ["Arnold", ...kittens]
+  return kittens
+}
+
